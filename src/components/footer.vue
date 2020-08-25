1c9bc4c736c0139e4e5fa62ec9f2c1854c8f7697
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-08-25 12:09:10 +0800

    Added icons, photos and footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
-    this is the footer
+  <div class="bg-black flex items-center" style="min-height: 40vh">
+    <div class="container mx-auto flex flex-col md:flex-row py-10 pl-8 xl:pl-0">
+      <div
+        class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/3 xl:w-1/2 flex justify-center items-center mb-8 md:md-0"
+      >
+        <img
+          src="../assets/images/logo/shifted-logo-white.png"
+          class="w-full lg:w-2/3 xl:w-1/2"
+        />
+      </div>
+      <div
+        class="text-white w-full md:w-3/4 lg:w-2/3 xl:w-1/2 flex flex-col sm:flex-row justify-center items-center shifted-light-gray-text minionpro-regular md:p-10 text-sm leading-snug"
+      >
+        <div class="w-full sm:w-1/2 mb-10 sm:mb-0">
+          The Official Photography Folio <br />
+          of the students of De La Salle-College <br />
+          of Saint Benilde. <br />
+          <br />
+          The Benildean Press Corps © 2020. <br />
+          All rights reserved.
+        </div>
+        <div class="w-full sm:w-1/2 text-sm">
+          <h2 class="chivo-bold text-2xl mb-2">STAY CONNECTED.</h2>
+          Address <br />
+          2/F Br. Miguel Febres Cordero Building, <br />
+          Taft Campus, De La Salle-College of Saint <br />
+          Benilde, Taft Avenue, Manila, Philippines
+          <div class="mt-5 ">
+            <a href="https://facebook.com/TheBenildean">
+              <img
+                src="../assets/images/icons/fb.png"
+                width="20px"
+                class="inline-block"
+              />
+            </a>
+            <a href="https://twitter.com/TheBenildean">
+              <img
+                src="../assets/images/icons/twt.png"
+                width="20px"
+                class="inline-block mx-1 "
+              />
+            </a>
+            <a href="https://instagram.com/TheBenildean">
+              <img
+                src="../assets/images/icons/ig.png"
+                width="20px"
+                class="inline-block"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
