0640d69860d5a264be3ab453490c36a935d3ea49
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-08-25 10:46:05 +0800

    Updated components

---
+++ src/components/navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/acknowledgement">acknowledgement</router-link> |
+    <router-link to="/entries">entries</router-link> |
+    <router-link to="/editors-note">editor's note</router-link> |
+    <router-link to="/staffbox">staffbox</router-link> |
+    <router-link to="/contributors">contributors</router-link>
+  </div>
+</template>
