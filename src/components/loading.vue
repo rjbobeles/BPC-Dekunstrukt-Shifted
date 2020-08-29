9e4ac8323a657b4cb5f8bf0ba608db9644061094
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-08-29 12:29:23 +0800

    Added loading component

---
+++ src/components/loading.vue
@@ -0,0 +1,68 @@
+<template>
+  <transition name="fade">
+    <div v-if="loading">
+      <div id="overlay">
+        <div id="logo">
+          <img src="../assets/images/logo.png" alt="Loading.." width="100px" />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      default: false,
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.fade-leave-active {
+  transition: opacity 1s;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+
+div#overlay {
+  position: fixed;
+  z-index: 99999;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgb(231, 231, 231);
+  transition: 250ms opacity;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  div#logo {
+    opacity: 1;
+    animation: opacity 2500ms infinite;
+    padding-bottom: 20px;
+  }
+
+  @keyframes opacity {
+    0% {
+      opacity: 1;
+    }
+
+    50% {
+      opacity: 0.2;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+}
+</style>
